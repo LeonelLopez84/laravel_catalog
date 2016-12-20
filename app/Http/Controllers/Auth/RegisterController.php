aab380596bd867a9852fc74650aa4256172258b3
Leonel López <leonel.lopez@webstage.com.mx>
2016-12-20 14:41:05 -0600

    cambio de nombre de la aplicacion

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Catalogo\Http\Controllers\Auth;
 
-use App\User;
+use Catalogo\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use Catalogo\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
